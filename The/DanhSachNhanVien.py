cb867fac9a17928122d0455c02b02b4b03da2684
tientien2512 <tien05153@gmail.com>
2023-04-30 11:57:50 +0700

    commit

--- The/DanhSachNhanVien.py
+++ The/DanhSachNhanVien.py
@@ -1,3 +1,19 @@
 import tkinter as tk
-import csv
-from QLNS_process import Nhanvien_TTca_nhan
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Widget Notebook")
+
+# Tạo widget Notebook
+notebook = ttk.Notebook(root)
+notebook.pack(fill='both', expand=True)
+
+# Tạo thẻ trong widget Notebook
+the = tk.Frame(notebook)
+
+# Thêm thẻ vào widget Notebook
+notebook.add(the, text="Danh sách nhân viên")
+
+tk.Label(the, text="Nội dung của thẻ").pack(pady=10)
+
+root.mainloop()
