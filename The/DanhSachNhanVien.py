e948dc6688344b3b05fafae75359c2e32b77c1b5
tientien2512 <tien05153@gmail.com>
2023-04-30 12:05:31 +0700

    commit

--- The/DanhSachNhanVien.py
+++ The/DanhSachNhanVien.py
@@ -2,9 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("Widget Notebook")
+root.title("Quản lý nhân sự")
 
-# Tạo widget Notebook
 notebook = ttk.Notebook(root)
 notebook.pack(fill='both', expand=True)
 
@@ -13,6 +12,10 @@ the = tk.Frame(notebook)
 
 # Thêm thẻ vào widget Notebook
 notebook.add(the, text="Danh sách nhân viên")
+
+dsnv = tk.Listbox(root, height=10)
+dsnv.pack(padx=10, pady=10)
+
 
 tk.Label(the, text="Nội dung của thẻ").pack(pady=10)
 
