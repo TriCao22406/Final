1b975beb86f9f411cf9e0354e779e1ab27538104
tientien2512 <tien05153@gmail.com>
2023-05-01 00:35:54 +0700

    commit

--- Tab/XoaSuaNhanVien.py
+++ Tab/XoaSuaNhanVien.py
@@ -1,11 +1,27 @@
 import tkinter as tk
 import csv
-from QLNS_process import Nhanvien_TTca_nhan
+from DanhSachNhanVien import *
+# chưa có xong bị lỗi rùi mai sửa tiếp
+root =tk.Tk()
+root.geometry('700x500')
+def xoa_nv():
+    manv_xoa = view.selection()[0]
+    values = view.item(manv_xoa)['values']
+    a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
+    with open(a, mode='r', newline='') as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+        for i, row in enumerate(rows):
+            if row == values:
+                rows.pop(i)
+                break
+    a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
+    with open(a, mode='r', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+    view.delete(manv_xoa)
 
-class HienthiDanhsach():
-    pass
-class XoaNhanVien():
-    pass
+delete_button = tk.Button(root, text="Xóa", command=xoa_nv)
+delete_button.pack()
 
-class SuaNhanVien():
-    pass
+root.mainloop()
