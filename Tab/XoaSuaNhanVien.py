509184735a9228d52c270c3478e5a71b72628037
tientien2512 <tien05153@gmail.com>
2023-05-01 12:39:20 +0700

    commit

--- Tab/XoaSuaNhanVien.py
+++ Tab/XoaSuaNhanVien.py
@@ -1,27 +1,36 @@
-import tkinter as tk
 import csv
-from DanhSachNhanVien import *
-# chưa có xong bị lỗi rùi mai sửa tiếp
-root =tk.Tk()
-root.geometry('700x500')
-def xoa_nv():
-    manv_xoa = view.selection()[0]
-    values = view.item(manv_xoa)['values']
-    a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
-    with open(a, mode='r', newline='') as f:
-        reader = csv.reader(f)
-        rows = list(reader)
-        for i, row in enumerate(rows):
-            if row == values:
-                rows.pop(i)
-                break
-    a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
-    with open(a, mode='r', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(rows)
-    view.delete(manv_xoa)
+import tkinter as tk
+from tkinter import ttk
+from DanhSachNhanVien import view
 
-delete_button = tk.Button(root, text="Xóa", command=xoa_nv)
+def xoanv():
+    nv_xoa = view.selection
+    if nv_xoa:
+        nv_in4 = view.item(nv_xoa)['values']
+        view.delete(nv_xoa)
+        a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
+        with open(a, mode='r', newline='') as file_csv:
+            csv_reader =  csv.reader(file_csv)
+            rows = list(csv_reader)
+        for i, row in enumerate(rows):
+            if row[0] == nv_in4[0] and row[1] == str(nv_in4[1]):
+                del row[i]
+                break
+        a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
+        with open(a, mode='r', newline='') as file_csv:
+            csv_writer = csv.writer(file_csv)
+            csv_writer.writerows(rows)
+
+root =tk.Tk()
+a = r"C:\Users\HP\Documents\GitHub\Final\database\employees.csv"
+with open(a, mode='r', newline='') as file_csv:
+    csv_reader =  csv.reader(file_csv)
+    next(csv_reader)
+    for row in csv_reader:
+        view.insert("", tk.END, values=row)
+
+view.pack()
+delete_button = tk.Button(root, text="Xóa", command=xoanv)
 delete_button.pack()
 
 root.mainloop()
